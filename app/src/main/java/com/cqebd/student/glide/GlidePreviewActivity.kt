723c012bb623465dfbe85ed4845196f7c2c7e988
diswy <formyselfdream@outlook.com>
2018-09-13 09:49:15 +0800

    去掉预览界面加载图片的缓存2

--- app/src/main/java/com/cqebd/student/glide/GlidePreviewActivity.kt
+++ app/src/main/java/com/cqebd/student/glide/GlidePreviewActivity.kt
@@ -1,15 +1,19 @@
 package com.cqebd.student.glide
 
+import android.Manifest
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.WindowManager
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
 import com.cqebd.student.R
 import com.cqebd.student.app.BaseActivity
+import com.cqebd.student.utils.DeleteHelper
 import com.orhanobut.logger.Logger
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.xiaofu.lib_base_xiaofu.img.GlideApp
 import com.xiaofu.lib_base_xiaofu.img.PhotoUtils
 import kotlinx.android.synthetic.main.activity_glide_preview.*
@@ -38,7 +42,7 @@ class GlidePreviewActivity : BaseActivity() {
                 , WindowManager.LayoutParams.FLAG_FULLSCREEN)
         imgPath = intent.getStringExtra("IMG_PATH")
         Logger.d("imgPath = $imgPath  degree = ${PhotoUtils.getPictureDegree(imgPath)}")
-        Sketch.with(this).display(imgPath,iv)
+        Sketch.with(this).display(imgPath, iv)
                 .disableCacheInDisk()
                 .disableCacheInMemory()
                 .disableBitmapPool()
@@ -48,6 +52,8 @@ class GlidePreviewActivity : BaseActivity() {
         GlideApp.with(this)
                 .asBitmap()
                 .load(imgPath)
+                .skipMemoryCache(true)// 不使用内存缓存
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(target)
 
     }
@@ -74,8 +80,18 @@ class GlidePreviewActivity : BaseActivity() {
                 setResult(888)
                 finish()
             } else {
-                finalBitmap = rotateBitmap(mBitmap, rotate)
-                saveBitmap()
+                RxPermissions(this).request(Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        , Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .subscribe { granted ->
+                            if (granted) {
+                                if (DeleteHelper.delete(imgPath)){
+                                    finalBitmap = rotateBitmap(mBitmap, rotate)
+                                    saveBitmap()
+                                }
+                            } else {
+                                toast("您拒绝了必要权限,请授权后尝试")
+                            }
+                        }
             }
         }
     }
@@ -101,16 +117,15 @@ class GlidePreviewActivity : BaseActivity() {
         if (isLoadSuccess) {
             val file = File(imgPath)
             if (!file.exists()) {
-                return
+                file.createNewFile()
             }
 
             try {
                 val fileOutputStream = FileOutputStream(file)
                 finalBitmap?.let {
-                    it.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+                    it.compress(Bitmap.CompressFormat.JPEG, 65, fileOutputStream)
                     fileOutputStream.flush()
                     fileOutputStream.close()
-
                     setResult(888)
                     finish()
                 }
